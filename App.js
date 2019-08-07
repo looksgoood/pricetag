828ef0ebc127c5199eab2e6b81a9bed290836629
Jaehyun Cho <looksgoood12@gmail.com>
2019-08-08 00:11:43 +0700

    Modify user profile

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { StyleSheet, View, ImageBackground, Image, Text } from 'react-native';
 import ImageIntent from 'react-native-image-intent';
 import { Navigation } from 'react-native-navigation';
+import AsyncStorage from '@react-native-community/async-storage';
 
 export default class App extends Component {
   state = {
@@ -21,20 +22,56 @@ export default class App extends Component {
     });
   }
 
-  componentDidMount() {
+  _loadLanding = () => {
+    Navigation.setStackRoot(this.props.componentId,
+    {
+      component: {
+          name: 'example.Landing',
+          passProps: {
+            images: this.state.images,
+            token: this.state.token,
+          },
+        },
+        options: {
+          animations: {
+            setStackRoot: {
+              enabled: true,
+            },
+          },
+        },
+    });
+  };
+
+  checkLogin = async () => {
+    const value = await AsyncStorage.getItem('@haetae:profile');
+    if (value !== null) {
+      console.log("login data exist");
+      this.checkImageIntent(true);
+    }
+    else {
+      this.checkImageIntent(false);
+    }
+  }
+
+  checkImageIntent = (loginFlag) => {
     ImageIntent.getImageIntentUrl().then((url) => {
       this.setState({
+        isLoaded: loginFlag,
         isLoaded: true,
         imageUris: url,
       });
     }).catch(e => {
       setTimeout(() => {
         this.setState({
+          isLoaded: loginFlag,
           isLoaded: true,
         });
       }, 1000);
     });
+  }
 
+  componentDidMount() {
+    this.checkLogin();
   }
 
   render() {
@@ -57,7 +94,10 @@ export default class App extends Component {
     );
     return (
       <View style={styles.container}>
-        {this.state.isLoaded ? this._loadLogin() : splash}
+        {this.state.isLoaded ? 
+          this.state.isLogin ? this._loadLanding() :
+            this._loadLogin() :
+          splash}
       </View>
     );
   }
